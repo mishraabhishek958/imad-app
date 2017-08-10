282e614e727973e4396b5e8d3b42784c1e156a72
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-11 03:33:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,17 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content {
+var articleOne = {
     title: 'Article-One |Abhishek',
     heading: 'Article-One',
     date:'Aug 10 , 2017',
-    content: ''
-    
-    
+    content: ` 
+        <p>
+            This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>
+        <p>
+            This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>
+        <p>
+            This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>`
+};
+function createTemplate(data) { 
+var title=data.title;
+var heading=data.heading;
+var date= data.date;
+var content= data.content;
+var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div >
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+           ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+         ${content}
+       </div> 
+    </body>
+</html>
+`;
+return htmlTemplate;
 }
-
-
-
 
 
 app.get('/', function (req, res) {
@@ -23,7 +60,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+  res.send(createTemplate(articleOne));
     
 });
 app.get('/article-two',function (req,res) {
