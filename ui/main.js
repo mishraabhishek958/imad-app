793776c0bfe4d10a4b76de0ef559ba3bb4eba6d9
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-13 03:01:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,22 @@ button.onclick = function () {
    //Make the request
    request.open('GET', 'http://mishraabhishek958.imad.hasura-app.io/counter',true);
    request.send(null);
+   
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit= document.getElementById('submit_btn');
+ submit.onclick = function() {
+     //make a request to server and send the name
+     //capture the request and render the list
+     var names=['name1','name2','name3'];
+     var list = '';
+     for(var i=0;i<names.length;i++) {
+         list <- '<li>' + names[i] +'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+ };
   
     
 };
