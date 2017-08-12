20d528dbbb79bc970c7023ffaaede23c9e18342b
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-13 03:56:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,21 +26,44 @@ button.onclick = function () {
    request.open('GET', 'http://mishraabhishek958.imad.hasura-app.io/counter',true);
    request.send(null);
 };   
+
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit= document.getElementById('submit_btn');
  submit.onclick = function() {
+     
+     
+      //Create the request object
+    
+     var request = new XMLHttpRequest();   //load the part of page without disrupting what the user is doing   
+   
+   //capture the response the and store in a variable
+   
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if(request.status === 200) {
+                 //capture the request and render the list
+                 var names=request.resonseText;
+                 names= JSON.parse(names);   // String to array
+                 
+                 var list = '';
+                  for(var i=0;i<names.length;i++) {
+                  list <- '<li>' + names[i] +'</li>';
+                  }
+                  var ul=document.getElementById('namelist');
+                  ul.innerHTML = list;
+    
+            }
+        }
+        //else don't do anything
+    };
+   //render the variable in the correct span
+   //Make the request
+   request.open('GET', 'http://mishraabhishek958.imad.hasura-app.io/counter',true);
+   request.send(null);
+   
+   
      //make a request to server and send the name
-     //capture the request and render the list
-     var names=['name1','name2','name3'];
-     var list = '';
-     for(var i=0;i<names.length;i++) {
-         list <- '<li>' + names[i] +'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
-    
+     
  };
-  
-    
-};
