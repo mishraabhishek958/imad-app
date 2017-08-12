f4f0352e43f7b883fcaf016d61da83e60b0eddd2
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-12 21:01:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles {
+var articles = {
 articleOne :{
     title: 'Article-One |Abhishek',
     heading: 'Article-One',
