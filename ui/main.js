bcbb125715c664913c8f81704da92171317e3922
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-12 23:28:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,16 @@ console.log('Fuck off');
 //Change the text of the id main-text
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
-
+var marginLeft = 0;
 //move the image
+function moveRight() {
+    marginLeft = marginLeft + 2;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 
 var img = document.getElementById('madi');
 madi.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight , 50);
+    
 };
