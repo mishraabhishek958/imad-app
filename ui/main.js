e4f982018bfed4a405a8518a71f3e0a5d9c4849e
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-12 23:16:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,4 +10,5 @@ element.innerHTML = 'New value';
 var img = document.getElementById('madi');
 madi.onclick = function() {
     img.style.marginLeft = '100px';
-}
+    img.style.marginRight = '100px';
+};
