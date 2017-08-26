b8b2469bbb64b18d573236ec142dc31401f9ee87
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-26 12:29:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/articles/:articleName',function (req,res) {
          res.status(500).send(err.toString());
      }  else {
            if(result.rows.length === 0) {
-               res.status(404).send('Article not found');
+               res.status(404).send('Article  found');
            } else {
                var articleData = result.rows[0];
                res.send(createTemplate(articleData));
