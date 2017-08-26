1d4eadc5755c70c0668b9240af9c1546162436e9
Abhishek Mishra <mishraabhishek958@gmail.com>
2017-08-26 11:02:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/test-db', function (req, res) {
    //return a response with the result
     pool.query(' SELECT * FROM test' , function (err, result) {
         if (err) {
-            res.status(500).send(err,toString());
+            res.status(500).send(err.toString());
         } else {
             res.send(JSON.stringyfy(result));
         }    
